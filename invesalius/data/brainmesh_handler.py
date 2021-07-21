90c49375d7abc47d92a212a4899648cba0cdf9ee
Sotodela <ana.sotodelacruz@gmail.com>
2021-07-21 13:51:20 +0300

    add getCenter and getNormal functions, create locator and add self.peel_centers and self.peel_normals variables, add to calculate normals and centers when calling getPeelActor (new peel)

--- invesalius/data/brainmesh_handler.py
+++ invesalius/data/brainmesh_handler.py
@@ -77,13 +77,18 @@ class Brain:
         currentPeel= self.mapImageOnCurrentPeel(currentPeel)
 
         newPeel = vtk.vtkPolyData()
+
         newPeel.DeepCopy(currentPeel)
+
+
+        self.peel_normals = vtk.vtkFloatArray()
+        self.peel_centers = vtk.vtkFloatArray()
         self.peel.append(newPeel)
         self.currentPeelActor = vtk.vtkActor()
         self.currentPeelActor.SetUserMatrix(affine_vtk)
         self.getCurrentPeelActor(currentPeel)
         self.peelActors.append(self.currentPeelActor)
-
+        self.locator = vtk.vtkCellLocator()  # This one will later find the triangle on the peel surface where the coil's normal intersect
         self.numberOfPeels = n_peels
         self.peelDown(currentPeel)
 
@@ -182,6 +187,12 @@ class Brain:
         # Set actor
         self.currentPeelActor.SetMapper(mapper)
 
+        self.currentPeel = self.peel[p]
+        self.locator.SetDataSet(self.peel[p])
+        self.locator.BuildLocator()
+        self.getCenters()
+        self.getNormals()
+
         return self.currentPeelActor
 
     def getCurrentPeelActor(self, currentPeel):
@@ -215,6 +226,26 @@ class Brain:
         self.currentPeelActor.GetProperty().SetOpacity(0.5)
 
         return self.currentPeelActor
+
+    def getCenters(self):
+        # Compute centers of triangles
+        centerComputer = vtk.vtkCellCenters()  # This computes centers of the triangles on the peel
+        centerComputer.SetInputData(self.currentPeel)
+        centerComputer.Update()
+        peel_centers = vtk.vtkFloatArray()  # This stores the centers for easy access
+        peel_centers = centerComputer.GetOutput()
+        self.peel_centers = peel_centers
+
+    def getNormals(self):
+        # Compute normals of triangles
+        normalComputer = vtk.vtkPolyDataNormals()  # This computes normals of the triangles on the peel
+        normalComputer.SetInputData(self.currentPeel)
+        normalComputer.ComputePointNormalsOff()
+        normalComputer.ComputeCellNormalsOn()
+        normalComputer.Update()
+        peel_normals = vtk.vtkFloatArray()  # This converts to the normals to an array for easy access
+        peel_normals = normalComputer.GetOutput().GetCellData().GetNormals()
+        self.peel_normals = peel_normals
 
 
 def cleanMesh(inp):
